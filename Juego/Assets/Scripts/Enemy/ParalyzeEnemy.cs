2d129c7c3015a8a17e03375a21e060f6c06f9ba0
Ian Isaak <irebollo24@alumnos.uaq.mx>
2024-05-24 08:50:32 -0600

    Battery Animation update

--- Juego/Assets/Scripts/Enemy/ParalyzeEnemy.cs
+++ Juego/Assets/Scripts/Enemy/ParalyzeEnemy.cs
@@ -10,6 +10,7 @@ public class ParalyzeEnemy : MonoBehaviour
     public int maxUses = 3; // Número máximo de usos
     private int currentUses = 0; // Contador de usos actuales
     public TMP_Text counterText; // Texto UI para mostrar el contador
+    public Animator batteryAnimator; // Referencia al componente Animator
 
     private void Awake()
     {
@@ -30,6 +31,7 @@ public class ParalyzeEnemy : MonoBehaviour
     {
         entradas.Acciones.Parálisis.performed += contexto => Paralisis(contexto);
         UpdateCounterText();
+        UpdateBatteryAnimation();
     }
 
     private void Paralisis(InputAction.CallbackContext callbackContext)
@@ -45,6 +47,7 @@ public class ParalyzeEnemy : MonoBehaviour
                     enemyAI.Paralyze(paralysisDuration);
                     currentUses++;
                     UpdateCounterText();
+                    UpdateBatteryAnimation();
                 }
             }
         }
@@ -61,11 +64,39 @@ public class ParalyzeEnemy : MonoBehaviour
             counterText.text = "Uses left: " + (maxUses - currentUses);
         }
     }
+    private void UpdateBatteryAnimation()
+    {
+        // Se asigna el animator
+        if (batteryAnimator != null)
+        {
+            // Se calcula el estado actual en base a los usos restantes
+            int batteryState = maxUses - currentUses;
+            switch (batteryState)
+            {
+                case 3:
+                    batteryAnimator.Play("Full");
+                    break;
+                case 2:
+                    batteryAnimator.Play("3-2");
+                    break;
+                case 1:
+                    batteryAnimator.Play("2-1");
+                    break;
+                case 0:
+                    batteryAnimator.Play("1-0");
+                    break;
+                default:
+                    Debug.LogWarning("Invalid battery state.");
+                    break;
+            }
+        }
+    }
 
     // Esta función debe ser llamada cuando se reinicia el nivel
     public void ResetUses()
     {
         currentUses = 0;
         UpdateCounterText();
+        UpdateBatteryAnimation();
     }
 }
