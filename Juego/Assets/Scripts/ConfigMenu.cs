35530a5419c4e92abf6a594cfff956df106241f6
Ian Isaak <irebollo24@alumnos.uaq.mx>
2024-06-04 11:21:49 -0600

    Audio Configuration fiex and BGM added

--- Juego/Assets/Scripts/ConfigMenu.cs
+++ Juego/Assets/Scripts/ConfigMenu.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class ConfigMenu : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider volumeSlider;
+    private const string VolumePrefKey = "GameVolume";
+
+    private void Start()
+    {
+        float defaultVolume = 0.0f;
+        float savedVolume = PlayerPrefs.GetFloat(VolumePrefKey, defaultVolume);
+        audioMixer.SetFloat("Volume", savedVolume);
+
+        if (volumeSlider != null)
+        {
+            volumeSlider.value = savedVolume;
+        }
+    }
+
     public void ChangeVolume(float volume)
     {
         audioMixer.SetFloat("Volume", volume);
+        PlayerPrefs.SetFloat(VolumePrefKey, volume);
+        PlayerPrefs.Save();
     }
 }
