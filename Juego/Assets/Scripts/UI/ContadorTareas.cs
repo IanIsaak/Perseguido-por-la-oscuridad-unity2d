4221a600c314c8745614dbf2ad439229a418e9ab
J Antonio Herrera <jherrera51@alumnos.uaq.mx>
2024-06-05 03:14:21 -0600

    feat task counter

---
+++ Juego/Assets/Scripts/UI/ContadorTareas.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ContadorTareas : MonoBehaviour
+{
+    private float tareas = 0;
+    private TextMeshProUGUI textTareas;
+    private VictoryDoorTrigger victoryDoorTrigger; // Referencia al VictoryDoorTrigger
+
+    void Start()
+    {
+        textTareas = GetComponent<TextMeshProUGUI>();
+
+        // Buscar VictoryDoorTrigger en la escena
+        victoryDoorTrigger = FindObjectOfType<VictoryDoorTrigger>();
+
+        if (victoryDoorTrigger == null)
+        {
+            Debug.LogError("No se encontró VictoryDoorTrigger en la escena.");
+        }
+    }
+
+    private void Update()
+    {
+        textTareas.text = tareas.ToString() + "/3 Tareas";
+
+    }
+
+    public void SumarPuntos()
+    {
+        tareas++;
+        if (victoryDoorTrigger != null && tareas == 3)
+        {
+            victoryDoorTrigger.OpenVictoryDoor(); // Llama al método para abrir la puerta
+        }
+    }
+}
